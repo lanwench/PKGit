cda4b28b06e1f20fe52695ec4e9a2939cb321bf2
Paula Kingsley <pkingsley@gracenote.com>
2016-06-05 12:19:50 -0700

    Still testing invoke-pkgitcommit -v

--- Scripts/Invoke-PKGitCommit.ps1
+++ Scripts/Invoke-PKGitCommit.ps1
@@ -2,10 +2,16 @@
 Function Invoke-PKGitCommit {
 <#
 .SYNOPSIS 
-    Invokes git commit
+    Uses invoke-expression and "git commit" with optional parameters in the current directory
 
 .DESCRIPTION
-    Uses invoke-expression and "git commit" with optional parameters
+    Uses invoke-expression and "git commit" with optional parameters in the current directory,
+    including add all tracked changes (git commit -a). 
+    First verifies that directory contains a repo. 
+    Forces mandatory message. 
+    Optional parameter invokes Invoke-PKGitPush and runs git-push if the commit
+    was successful and there are no untracked files.
+    Supports ShouldProcess.
     Requires git, of course.
 
 .NOTES
@@ -16,11 +22,13 @@ Function Invoke-PKGitCommit {
     
         ** PLEASE KEEP $VERSION UPDATED IN PROCESS BLOCK **
 
-        v1.0.0 - 2016-05-29 - Created script
+        v1.0.0 - 2016-06-05 - Created script
         
 
 .EXAMPLE
-    PS C:\> Invoke-PKGitCommit -Message "Updated some things" -AddAllTrackedChanges -Verbose
+    PS C:\MyRepo> Invoke-PKGitCommit -Message "Updated some things" -AddAllTrackedChanges -Verbose
+    # Runs git commit -a -m "Updated some things" -v 
+
         VERBOSE: PSBoundParameters: 
 	
         Key                  Value                                          
@@ -38,6 +46,85 @@ Function Invoke-PKGitCommit {
 
         VERBOSE: Add all changes/stage all tracked files, and invoke 'git commit -m "Updated some things -v"' ?
 
+        [master 6e5810a] Testing invoke-pkgitcommit after adding -v
+        1 file changed, 211 insertions(+)
+        create mode 100644 Scripts/MyFile.PS1
+
+.EXAMPLE
+    PS C:\MyRepo> Invoke-PKGitCommit -Message "Updated some other things" -Quiet
+    # Runs git commit -a -m "Updated some things" and returns a boolean 
+    
+        True        
+
+.EXAMPLE
+    PS C:\MyRepo> Invoke-PKGitCommit -Message "Testing some stuff" -AddAllTrackedChanges -InvokeGitPush -Verbose
+    # Runs git commit -a -m "Testing some stuff" and runs Invoke-PKGitPush if no untracked files are present
+        
+        VERBOSE: PSBoundParameters: 
+	
+        Key                  Value                                
+        ---                  -----                                
+        Message              Testing some stuff    
+        AddAllTrackedChanges True                                 
+        InvokeGitPush        True                                 
+        Verbose              True                                 
+        Quiet                False                                
+        Confirm              False                                
+        Path                 C:\MyRepo
+        ComputerName         PKINGSLEY-04343                      
+        ScriptName           Invoke-PKGitCommit                   
+        ScriptVersion        1.0.0                                
+
+
+        VERBOSE: Add all changes/stage all tracked files, and invoke 'git commit -m "Testing some stuff" -v' ?
+
+        On branch master
+        Your branch is ahead of 'origin/master' by 1 commit.
+          (use "git push" to publish your local commits)
+        nothing to commit, working directory clean
+
+        VERBOSE: PSBoundParameters: 
+	
+        Key           Value                                
+        ---           -----                                
+        Verbose       True                                 
+        Quiet         False                                
+        Confirm       False                                
+        Path          C:\MyRepo
+        ComputerName  PKINGSLEY-04343                      
+        ScriptName    Invoke-PKGitPush                     
+        ScriptVersion 1.0.0                                
+
+
+        Push URL: https://github.com/JoeBloggs/myrepo.git
+
+        VERBOSE: Invoke 'git push -v' from the current repo 'C:\MyRepo' to remote origin 'https://github.com/JoeBloggs/myrepo.git'?
+        VERBOSE: Redirecting output streams.
+        To https://github.com/JoeBloggs/myrepo.git
+           01d597b..6e5810a  master -> master
+
+.EXAMPLE
+    PS C:\MyRepo> Invoke-PKGitCommit -Message "I like coffee" -AddAllTrackedChanges -Verbose -InvokeGitPush
+    # Runs git commit -a -m "I like coffee" and does not run Invoke-PKGitPush due to untracked files
+
+        VERBOSE: PSBoundParameters: 
+	
+        Key                  Value                                          
+        ---                  -----                                          
+        Message              I like coffee
+        AddAllTrackedChanges True                                           
+        Verbose              True                                           
+        InvokeGitPush        True                                           
+        Quiet                False                                          
+        Confirm              False                                          
+        Path                 C:\MyRepo         
+        ComputerName         PKINGSLEY-04343                                
+        ScriptName           Invoke-PKGitCommit                             
+        ScriptVersion        1.0.0                                          
+
+
+        VERBOSE: Add all changes/stage all tracked files, and invoke 'git commit -m "I like coffee" -v"' ?
+
         On branch master
         Your branch is up-to-date with 'origin/master'.
         Untracked files:
@@ -45,32 +132,7 @@ Function Invoke-PKGitCommit {
 
         nothing added to commit but untracked files present
 
-.EXAMPLE
-    PS C:\> Invoke-PKGitCommit -Message "Updated some other things" -Quiet
-        VERBOSE: PSBoundParameters: 
-	
-        Key                  Value                                          
-        ---                  -----                                          
-        Message              Updated some things
-        AddAllTrackedChanges True                                           
-        InvokeGitPush        False                                          
-        Verbose              True                                           
-        Quiet                False                                          
-        Confirm              False                                          
-        Path                 C:\MyRepo          
-        ComputerName         PKINGSLEY-04343                                
-        ScriptName           Invoke-PKGitCommit                             
-        ScriptVersion        1.0.0                                          
-
-        VERBOSE: Invoke 'git commit -m "Updated some other things"' ?
-
-        On branch master
-        Your branch is up-to-date with 'origin/master'.
-        Untracked files:
-	        Scripts/NewFile.ps1
-
-        nothing added to commit but untracked files present        
-
+        WARNING: 'C:\MyRepo' contains untracked files; will not invoke git-push
 
 
 #>
